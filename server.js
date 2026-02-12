fbbbd11d98b11351e58b8a692ead5c483f72704c
koppuhemanthsaikumar <koppuhemanthsaikumar123@gmail.com>
2026-02-12 23:54:00 +0530

    room feature added

--- server.js
+++ server.js
@@ -8,35 +8,56 @@ const io = new Server(server);
 
 app.use(express.static("public"));
 
-const users = {}; // socket.id -> username
+const users = {}; // socket.id -> { username, room }
+
+function getRoomUsers(room) {
+  return Object.values(users)
+    .filter(user => user.room === room)
+    .map(user => user.username);
+}
 
 io.on("connection", (socket) => {
   console.log("New user connected:", socket.id);
 
-  socket.on("user-joined", (username) => {
-    users[socket.id] = username;
+  socket.on("join-room", ({ username, room }) => {
+  users[socket.id] = { username, room };
 
-    io.emit("system-message", `${username} joined the chat`);
-  });
+  socket.join(room);
+
+  io.to(room).emit("system-message", `${username} joined ${room}`);
+
+  //Send updated user list
+  io.to(room).emit("room-users", getRoomUsers(room));
+});
+
 
   socket.on("chat-message", (msg) => {
+    const user = users[socket.id];
+    if (!user) return;
+
     const messageData = {
-      user: users[socket.id],
+      user: user.username,
       message: msg,
       time: new Date().toLocaleTimeString()
     };
 
-    io.emit("chat-message", messageData);
+    io.to(user.room).emit("chat-message", messageData);
   });
 
   socket.on("disconnect", () => {
-    const username = users[socket.id];
-    if (username) {
-      io.emit("system-message", `${username} left the chat`);
-      delete users[socket.id];
-    }
-  });
+  const user = users[socket.id];
+  if (user) {
+    io.to(user.room).emit("system-message", `${user.username} left the room`);
+
+    delete users[socket.id];
+
+    // Update users after leaving
+    io.to(user.room).emit("room-users", getRoomUsers(user.room));
+  }
 });
+
+});
+
 
 server.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
